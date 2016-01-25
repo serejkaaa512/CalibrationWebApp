12ce81284c834f6104560cfccd2db69ae69c3506
serejkaaa512 <5125402@mail.ru>
2016-01-25 16:48:14 +0300

    first successful test!

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@
 extern crate nickel;
 extern crate calibration_web_app;
 
-use nickel::Nickel;
+use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Calibration start!"
-        }
-    });
+    server.get("/", middleware!("<html><title>Calibration!!!</title></html>"));
 
     server.listen("127.0.0.1:6767");
+}
+
+
+#[test]
+fn server_test() {
+    assert!(true);
 }
